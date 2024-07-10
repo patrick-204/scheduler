9d514e8951c1a98cebcaee05a27ba4cdd3ffadbb
patrick-204 <p.ohara921@gmail.com>
2024-07-09 22:11:22 -0400

    Add moch data for fetching from axios API. Add test assertions to Application component.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,3 +9,18 @@ afterEach(cleanup);
 it("renders without crashing", () => {
   render(<Application />);
 });
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { findByText } = render(<Application />);
+
+  return findByText("Monday");
+});
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
